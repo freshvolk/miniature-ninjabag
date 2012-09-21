d6b6c34c0377a2b4916d2eac88e7c0c1c958325e
Brian Volk <freshvolk@gmail.com>
2012-09-21 00:33:24 -0700

    Ooops
    Forgot to commit for a while....
    
    Changed look to what it will be looking like at end
    Still issues with Request.JSON
    SQL seems to be working.

--- js/grabber.js
+++ js/grabber.js
@@ -1,100 +1,99 @@
-var devices = [{"device":" Game Boy colour","areas":[]},{"device":"04 1994 jeep grand cherokee install ign key cyl","areas":[]},{"device":"1959 Vespa 150","areas":[]},{"device":"1977 Columbia Commuter","areas":["Motorcycle"]},{"device":"1982-1988 Volvo 740","areas":[]},{"device":"1984-1988 Toyota Pickup","areas":["Toyota Automobile"]},{"device":"1984-1989 Toyota Pickup","areas":[]},{"device":"1984-1991 BMW 3-Series","areas":["BMW Automobile"]},{"device":"1985-1988 Volvo 740","areas":["Volvo Automobile"]},{"device":"1986-1993 Volvo 240","areas":["Volvo Automobile"]},{"device":"1987-1993 Kawasaki Ninja 500","areas":["Motorcycle"]},{"device":"1988-1991 Honda Civic","areas":["Honda Automobile"]},{"device":"1988-1994 Toyota Pickup","areas":[]},{"device":"1988-1998 Chevrolet Pickup","areas":["Chevrolet Automobile"]},{"device":"1988-1998 Chevy Pickup","areas":[]},{"device":"1988-1998 Chevy Silverado","areas":[]},{"device":"1989-1994 Mazda Protege","areas":[]},{"device":"1989-1994 Subaru Legacy","areas":["Subaru Automobile"]},{"device":"1989-1994 Toyota Pickup","areas":["Toyota Automobile"]},{"device":"1990 BMW 325i","areas":[]}];
-
-var gear_bag = [];
-var elements = 0;
+var dude = 12;
 
 window.addEvent('domready', function(){
 
-	var page = 0
-	var offset = 20*page;
-	var apiCall = 'http://ifixit.com/api/0.1/devices?';
-	var apiOpt = 'offset=' + offset + '&limit=20';
-	
-	/*
-	var jsonreq = new Request.HTML({
-		url: apiCall,
-		onRequest: function(){
-			console.log('Requesting page: ' + page);
-		},
-		onSuccess: function(tree, ele, html, js){
-			console.log('Success');
-			console.log(text);
-	
-			
-		},
-		onError: function(test, error) {
-			console.log(error);
-		},
-		onFailure: function(xhr){
-			console.log('Failed');
-			console.log(xhr);
-		}
-	});
-	
-	jsonreq.post(apiOpt);
-	*/
-	loadMore(0);
+	loadFromDB();
 
 });
 
-function populate(list) {
-	
-	var deviceList = [];
-	Array.each(list, function(dev){
-		var temp = new Element('div', { 
-		
-			'class': 'device',
-			
-			html: dev.device
-		})
-		
-		var tempFx = new Fx.Tween(temp);
-		// temp.addEvent('mouseDown', function(){
-			// tempFx.set('position','absolute');
-			// });
-		
-		var drag = new Drag.Move(temp, {
-		
-			droppables: '#grab',
-			
-			onDrop: function(element, droppable, event){
-				if (!droppable) console.log(element, ' dropped on nothing');
-				else {
-					console.log(element, 'dropped on', droppable);
-					saveToHTML(element);
+function loadFromDB(){
+	gearDB.findA('gear',addDevices);
+}
+
+function addDevices(transaction,result){
+	var toAdd = []
+	var bag = $('bag');
+	if (result.rows.length === 0) {
+		loadFromAPI(0);
+	} else {
+		for (var i = result.rows.length - 1; i >= 0; i--){
+				if (result.rows.item(i).in_bag === true){
+					if (bag.contains($('deleteMe'))){
+						$('deleteMe').dispose();
+					}
+					bag.adopt(new Element('div',{
+						'class' : 'item',
+						html : result.rows.item(i).device + ' ' + result.rows.item(i).device_id
+					}));
+				} else {
+					toAdd.append([new Element('div',{
+						'class' : 'device',
+						html : "<img class='device' src='" + result.rows.item(i).device_pic + "'/><p>" + result.rows.item(i).device + "</p>"
+					})]);
 				}
-			},
- 
-		    onEnter: function(element, droppable){
-		        console.log(element, 'entered', droppable);
-    		},
- 
-    		onLeave: function(element, droppable){
-        		console.log(element, 'left', droppable);
-    		}
-		});
-		deviceList.append([temp]);
+		}
+		$('devices').adopt(toAdd);
+		loadFromAPI(result.rows.length);
+	}
+}
+
+function loadFromAPI(offset) {
+	var jsonReq = new Request.JSON({
+		url: 'http://www.ifixit.com/api/0.1/devices?',
+		onComplete: function(datum){
+			//Temp data...JSON req still failing for unknown reason
+			var data = [{"device":" Game Boy colour","areas":[]},{"device":"04 1994 jeep grand cherokee install ign key cyl","areas":[]},{"device":"1959 Vespa 150","areas":[]},{"device":"1977 Columbia Commuter","areas":["Motorcycle"]},{"device":"1982-1988 Volvo 740","areas":[]},{"device":"1984-1988 Toyota Pickup","areas":["Toyota Automobile"]},{"device":"1984-1989 Toyota Pickup","areas":[]},{"device":"1984-1991 BMW 3-Series","areas":["BMW Automobile"]},{"device":"1985-1988 Volvo 740","areas":["Volvo Automobile"]},{"device":"1986-1993 Volvo 240","areas":["Volvo Automobile"]},{"device":"1987-1993 Kawasaki Ninja 500","areas":["Motorcycle"]},{"device":"1988-1991 Honda Civic","areas":["Honda Automobile"]},{"device":"1988-1994 Toyota Pickup","areas":[]},{"device":"1988-1998 Chevrolet Pickup","areas":["Chevrolet Automobile"]},{"device":"1988-1998 Chevy Pickup","areas":[]},{"device":"1988-1998 Chevy Silverado","areas":[]},{"device":"1989-1994 Mazda Protege","areas":[]},{"device":"1989-1994 Subaru Legacy","areas":["Subaru Automobile"]},{"device":"1989-1994 Toyota Pickup","areas":["Toyota Automobile"]},{"device":"1990 BMW 325i","areas":[]}]
+			
+
+			for (var i = data.length - 1; i >= 0; i--) {
+				//Will be replaced by function that gets image from API
+				var picture = 'http://guide-images.ifixit.net/igi/JJx6Cg1ePt6vAoDn';
+				
+				var values = {'device' : data[i].device, 'device_pic' : picture, 'in_bag' : false};
+				gearDB.insert('gear',values,callback);
+			};
+		} 
 	});
-	//console.log(deviceList);
-	$('devices').adopt(deviceList);
-	
+	jsonReq.get({'offset' : offset, 'limit' : 25});
 }
 
-function loadMore(page) {
-	//$('loadem').fade('out');
-	//$('more').dispose();
-	populate(devices);
-	// $('devices').adopt(new Element('div', {
-		// id: 'more',
-		// html: 'Click me!',
-		// events: {
-			// click: loadMore(++page)
-		// }
-	// }));
+function addToDeviceList(transaction,result){
+	$('devices').adopt(new Element('div',{
+		'class' : 'device',
+		'id' : result.insertId,
+		html : "<img class='device' src='" + values['device_pic'] + "'/><p>" + data[i].device + "</p>",
+		events: {
+			mousedown: function(event){
+				event.stop();
+
+				var dragged = this.clone().setStyles(clone.getCoordinates()).setStyles({
+					opacity: .7,
+					position : 'absolute'
+				}).inject($('app'));
+
+				var drag = new Drag.Move(dragged, {
+					droppables: $('bag'),
+
+					onDrop: function(dragging,bag){
+						dragging.destroy();
+						gearDB("UPDATE 'gear' SET in_bag = true WHERE device_id = " + result.insertId, addToBag.bindWithEvent());
+						
+					},
+
+					onCancel: function(dragging){
+						dragging.destroy();
+					}
+				});
+				drag.start(event);
+			}
+		}
+	}));
+
 }
 
-function saveToHTML(element) {
-	gear_bag.append([element.tween('height',25)]);
-	element.destroy();
-	$('grab').adopt(element.tween('width','90%'));
+function addToBag(transaction,result){
+	$('bag').adopt(new Element('div',{
+		'class' : 'item',
+		html : result.rows.item(i).device + ' ' + result.rows.item(i).device_id
+	}));
 }
--- js/grabbersql.js
+++ js/grabbersql.js
@@ -1,10 +1,3 @@
-window.addEvent('domready', function(){
-	
-	console.log('SQL stuff should occur...now');
-	test('iMac',9,'Bleh');
-	
-});
-
 //Start new instance
 var gearDB = new MooSQL({
 		dbName: 'gear_bag',
@@ -13,23 +6,31 @@ var gearDB = new MooSQL({
 		dbSize: 40
 });
 
+//This isn't firing for some reason
 gearDB.addEvent('databaseCreated',function(){
 	// Create table
-	gearDB.exec("CREATE TABLE gear(device TEXT NOT NULL, device_id UNIQUE INTEGER NOT NULL, device_pic BLOB)");
+	gearDB.exec("CREATE TABLE gear(device TEXT NOT NULL, device_id INTEGER PRIMARY KEY, device_pic TEXT, in_bag BOOLEAN DEFAULT false)",callback.bindWithEvent());
 	console.log('New DB');
-})
-	
-gearDB.addEvent('notSupported', function(ex){
-	console.log('no Support');
 });
-	
+
 function callback(transaction, result){
-	console.log(transaction + " and the result " + result);
+	console.log(transaction + " and the result ");
+	console.log(result);
+
 };
 
-function test(device,id,picture){
-	gearDB.exec("INSERT INTO gear (device, device_id) VALUES ('iMac',3)",callback.bindWithEvent());
-	gearDB.exec("SELECT * FROM 'gear'",callback.bindWithEvent());
-	
+function callbackRows(transaction, result){
+	console.log(transaction + " and the result ");
+	console.log(result.rows.item(0));
+	//refreshGearBag();
+};
+
+function addToBag(device,id,picture){
+	var values = {'device' : device, 'device_id' : id, 'device_pic' : picture, 'in_bag' : true};
+	gearDB.insert('gear',values,callback);
+	gearDB.exec("SELECT * FROM 'gear'",callbackRows.bindWithEvent());
 }
-	
+
+/*function addToDB(device){
+
+}*/
